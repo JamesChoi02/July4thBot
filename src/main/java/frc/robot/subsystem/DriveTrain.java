ddd790537f81e7a128c5086d8b2443b565ea9ee6
Alex Herridge <alherridge@gmail.com>
2019-03-09 16:59:12 -0500

    Added Method Stubs

--- src/main/java/frc/robot/subsystem/DriveTrain.java
+++ src/main/java/frc/robot/subsystem/DriveTrain.java
@@ -1,6 +1,22 @@
 package frc.robot.subsystem;
 
-public class DriveTrain
-{
+import edu.wpi.first.wpilibj.command.Subsystem;
 
+public class DriveTrain extends Subsystem {
+    public void tankDrive(double left, double right) {
+
+    }
+
+    public void driveStraight(double speed) {
+
+    }
+
+    public double getAngle() {
+        return 0;
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
 }
