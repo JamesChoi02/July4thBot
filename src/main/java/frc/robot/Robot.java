e2874620e978391c52cbe7e68ef8859d3c1c5ba2
jrAlex <alherridge@gmail.com>
2019-03-10 18:07:44 -0400

    Switched to 4 Space Indent

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.*;
-
 import badlog.lib.BadLog;
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -21,64 +20,61 @@ import java.text.SimpleDateFormat;
  * directory.
  */
 public class Robot extends TimedRobot {
+    private BadLog logger;
 
-  private BadLog logger;
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd-HH-mm").format(new Date());
+        logger = BadLog.init("/home/lvuser/" + timestamp + ".badbag");
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    String timestamp = new SimpleDateFormat("yyyy-MM-dd-HH-mm").format(new Date());
-    logger = BadLog.init("/home/lvuser/" + timestamp + ".badbag");
+        BadLog.createValue("OS Version", System.getProperty("os.version"));
 
-    BadLog.createValue("OS Version", System.getProperty("os.version"));
+        BadLog.createValue("Match Type", DriverStation.getInstance().getMatchType().toString());
+        BadLog.createValue("Match Number", "" + DriverStation.getInstance().getMatchNumber());
+        BadLog.createTopic("Match Time", "s", DriverStation.getInstance()::getMatchTime);
 
-    BadLog.createValue("Match Type", DriverStation.getInstance().getMatchType().toString());
-    BadLog.createValue("Match Number", "" + DriverStation.getInstance().getMatchNumber());
-    BadLog.createTopic("Match Time", "s", DriverStation.getInstance()::getMatchTime);
+        logger.finishInitialization();
+    }
 
+    /**
+     * This function is run once each time the robot enters autonomous mode.
+     */
+    @Override
+    public void autonomousInit() {
+    }
 
-    logger.finishInitialization();
-  }
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        teleopPeriodic();
+    }
 
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
-  @Override
-  public void autonomousInit() {
-  }
+    /**
+     * This function is called once each time the robot enters teleoperated mode.
+     */
+    @Override
+    public void teleopInit() {
+    }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    teleopPeriodic();
-  }
+    /**
+     * This function is called periodically during teleoperated mode.
+     */
+    @Override
+    public void teleopPeriodic() {
+        logger.updateTopics();
+        logger.log();
+    }
 
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
-  @Override
-  public void teleopInit() {
-    
-  }
-
-  /**
-   * This function is called periodically during teleoperated mode.
-   */
-  @Override
-  public void teleopPeriodic() {
-    logger.updateTopics();
-    logger.log();
-  }
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
