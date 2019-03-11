6d4ebd547aeeb3c89b1d538402da05c01694438d
jrAlex <alherridge@gmail.com>
2019-03-10 20:09:12 -0400

    Added NAVX Auto Log

--- src/main/java/frc/robot/util/AutoLogger.java
+++ src/main/java/frc/robot/util/AutoLogger.java
@@ -1,21 +1,24 @@
 package frc.robot.util;
 
 import badlog.lib.BadLog;
+
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import frc.robot.drivers.CANTalonSRX;
 
 public class AutoLogger {
     public static void log(String subsystemName, String deviceName, Object device) {
-        if (device instanceof CANSparkMax)
-            logSpark(subsystemName, deviceName, (CANSparkMax) device);
-        else if (device instanceof CANTalonSRX)
-            logTalon(subsystemName, deviceName, (CANTalonSRX) device);
-        else
-            throw new UnsupportedOperationException(
-                    "Device of type " + device.getClass().getName() + " has no auto log implementation");
+        try {
+            AutoLogger.class.getDeclaredMethod("log", String.class, String.class, device.getClass()).invoke(null,
+                    subsystemName, deviceName, device);
+        } catch (NoSuchMethodException e) {
+            System.err.println("Device of type " + device.getClass().getName() + " has no auto log implementation");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void logSpark(String subsystemName, String deviceName, CANSparkMax spark) {
+    private static void log(String subsystemName, String deviceName, CANSparkMax spark) {
         String combo = subsystemName + "/" + deviceName;
 
         BadLog.createValue(combo + " Firmware", spark.getFirmwareString());
@@ -29,17 +32,28 @@ public class AutoLogger {
                 "join:" + subsystemName + "Output Temperatures");
     }
 
-    private static void logTalon(String subsystemName, String deviceName, CANTalonSRX talon) {
+    private static void log(String subsystemName, String deviceName, CANTalonSRX talon) {
         String combo = subsystemName + "/" + deviceName;
 
         BadLog.createValue(combo + " Firmware", "" + talon.getFirmwareVersion());
 
-        BadLog.createTopic(combo + " Output Percent", BadLog.UNITLESS, () -> talon.get(), "hide",
+        BadLog.createTopic(combo + " Output Percent", BadLog.UNITLESS, () -> talon.get(),
                 "join:" + subsystemName + "Output Percents");
 
-        BadLog.createTopic(combo + " Current", "A", () -> talon.getOutputCurrent(), "hide",
+        BadLog.createTopic(combo + " Current", "A", () -> talon.getOutputCurrent(),
                 "join:" + subsystemName + "Output Currents");
-        BadLog.createTopic(combo + " Temperature", "C", () -> talon.getTemperature(), "hide",
+        BadLog.createTopic(combo + " Temperature", "C", () -> talon.getTemperature(),
+                "join:" + subsystemName + "Output Temperatures");
+    }
+
+    private static void log(String subsystemName, String deviceName, AHRS ahrs) {
+        String combo = subsystemName + "/" + deviceName;
+
+        BadLog.createValue(combo + " Firmware", "" + ahrs.getFirmwareVersion());
+        BadLog.createTopic(combo + " Pitch", "deg", () -> (double) ahrs.getPitch());
+        BadLog.createTopic(combo + " Yaw", "deg", () -> (double) ahrs.getYaw());
+        BadLog.createTopic(combo + " Roll", "deg", () -> (double) ahrs.getRoll());
+        BadLog.createTopic(combo + " Temperature", "C", () -> (double) ahrs.getTempC(),
                 "join:" + subsystemName + "Output Temperatures");
     }
 }
