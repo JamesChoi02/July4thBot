05dcd2598e19b5ed13a8c4a3903fc3fd551c77e6
jrAlex <alherridge@gmail.com>
2019-03-10 20:08:41 -0400

    Updated RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,15 +4,16 @@ public class RobotMap {
     /**
      * Drivetrain Motors
      */
-    public static final int FRONT_LEFT_MOTOR = 4;
-    public static final int FRONT_RIGHT_MOTOR = 1;
-    public static final int BACK_LEFT_MOTOR = 2;
-    public static final int BACK_RIGHT_MOTOR = 3;
+    public static final int FRONT_LEFT_MOTOR = 2;
+    public static final int FRONT_RIGHT_MOTOR = 3;
+    public static final int BACK_LEFT_MOTOR = 4;
+    public static final int BACK_RIGHT_MOTOR = 1;
 
     /**
      * Aux Motors
      */
-    public static final int LIFTER_MOTOR = 10;
-    public static final int GRABBER_MOTOR = 12;
-    public static final int CLAW_MOTOR = 14;
+    public static final int LIFTER_MOTOR = 12;
+    public static final int CAM_MOTOR = 23;
+    public static final int ARTICULATOR_MOTOR = 25;
+    public static final int GRABBER_MOTOR = 21;
 }
